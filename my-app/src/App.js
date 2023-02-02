efa40fdb8e88ae41a48c433cd0ef279f149a8b39
nouhmaster <nouh-master@outlook.fr>
2023-02-02 20:59:39 +0100

    first push

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <form>
+  <label>
+    Nom :
+    <input type="text" name="name" />
+  </label>
+  <input type="submit" value="Envoyer" />
+</form>
+    </div>
+  );
+}
+
+export default App;
