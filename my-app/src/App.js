028b4f888a4ec7e4adc3245edb5a24ab46115c61
nouhmaster <nouh-master@outlook.fr>
2023-02-02 22:12:40 +0100

    carré ça marche

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,25 @@
-import logo from './logo.svg';
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Header from "./Header";
 
-import './App.css';
+
 
 function App() {
+  const { register, handleSubmit } = useForm();
+  const [data, setData] = useState("");
   return (
-    <div className="App">
-        <form>
-  <label>
-    Nom :
-    <input type="text" name="name" />
-  </label>
-  <input type="submit" value="Envoyer" />
-</form>
-    </div>
+    <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
+      <Header />
+      <input {...register("firstName")} placeholder="First name" />
+      <select {...register("category", { required: true })}>
+        <option value="">Select...</option>
+        <option value="A">Option A</option>
+        <option value="B">Option B</option>
+      </select>
+      <textarea {...register("aboutYou")} placeholder="About you" />
+      <p>{data}</p>
+      <input type="submit" />
+    </form>
   );
 }
 
